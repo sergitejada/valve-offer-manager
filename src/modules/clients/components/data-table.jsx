7ae6e66b2168b10ba23db964260442c8a0e2e1d9
Sergi Tejada <sergitejada0@gmail.com>
2024-09-07 12:30:42 +0200

    Refactor DataTable component to include DataTableToolbar for filtering clients

--- src/modules/clients/components/data-table.jsx
+++ src/modules/clients/components/data-table.jsx
@@ -1,35 +1,36 @@
+import DataTableToolbar from '@modules/clients/components/data-table-toolbar'
 import PropTypes from 'prop-types'
 
-import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from '@/modules/commons/ui/table'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/modules/commons/ui/table'
 
 export default function DataTable({ data }) {
   return (
-    <Table className="border">
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data.map((invoice) => (
-          <TableRow key={invoice.invoice}>
-            <TableCell className="font-medium">{invoice.invoice}</TableCell>
-            <TableCell>{invoice.paymentStatus}</TableCell>
-            <TableCell>{invoice.paymentMethod}</TableCell>
-            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right">$2,500.00</TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
+    <div className="space-y-4">
+      <DataTableToolbar />
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-full">Name</TableHead>
+              <TableHead className="whitespace-nowrap text-right">Active Orders</TableHead>
+              <TableHead className="whitespace-nowrap text-right">Total Orders</TableHead>
+              <TableHead className="whitespace-nowrap text-right">Last Order</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.map((client) => (
+              <TableRow key={client.id}>
+                <TableCell className="font-medium">{client.name}</TableCell>
+                {/* <TableCell>{client.contact.size}</TableCell> */}
+                <TableCell className="text-right">{client.active_order}</TableCell>
+                <TableCell className="text-right">{client.total_order}</TableCell>
+                <TableCell className="text-right text-blue-500 underline">{client.last_order}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
   )
 }
 
