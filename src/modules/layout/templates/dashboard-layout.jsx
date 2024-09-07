bb3688ced4ca883b50fa1d1a6989d2038d4f2d38
Sergi Tejada <sergitejada0@gmail.com>
2024-09-07 12:31:07 +0200

    Refactor Sidebar component to use lucide-react icons and update styling

--- src/modules/layout/templates/dashboard-layout.jsx
+++ src/modules/layout/templates/dashboard-layout.jsx
@@ -1,20 +1,23 @@
-import ClientsIcon from '@modules/commons/icons/clients-icon'
-import DashboardIcon from '@modules/commons/icons/dashboard-icon'
-import OffersIcon from '@modules/commons/icons/offers-icon'
+// import ClientsIcon from '@modules/commons/icons/clients-icon'
+// import DashboardIcon from '@modules/commons/icons/dashboard-icon'
+// import OffersIcon from '@modules/commons/icons/offers-icon'
 import Sidebar, { SidebarItem } from '@modules/layout/components/sidebar'
+import { Calculator, LayoutDashboard, Users } from 'lucide-react'
 import PropTypes from 'prop-types'
 
 export default function DashboardLayout({ children }) {
   return (
-    <div className="flex h-screen w-full bg-gray-100">
-      <div className="h-screen">
-        <Sidebar>
-          <SidebarItem icon={<DashboardIcon />} text="Dashboard" href="/" />
-          <SidebarItem icon={<ClientsIcon />} text="Clients" href="/clients" />
-          <SidebarItem icon={<OffersIcon />} text="Offers" href="/offers" />
+    <div className="flex h-screen w-full overflow-hidden bg-zinc-50">
+      <div className="h-full">
+        <Sidebar className="h-full">
+          <SidebarItem icon={<LayoutDashboard />} text="Dashboard" href="/" />
+          <SidebarItem icon={<Users />} text="Clients" href="/clients" />
+          <SidebarItem icon={<Calculator />} text="Offers" href="/offers" />
         </Sidebar>
       </div>
-      <div className="w-full bg-gray-100 px-16 py-8">{children}</div>
+      <div className="flex-1 overflow-auto">
+        <div className="px-16 py-8">{children}</div>
+      </div>
     </div>
   )
 }
