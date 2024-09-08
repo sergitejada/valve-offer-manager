b8e8072471cd97033ed9c1540ca8a9fa684bd019
Sergi Tejada <sergitejada0@gmail.com>
2024-09-08 10:44:34 +0200

    Refactor DataTable component to include client sheet functionality

--- src/modules/clients/components/data-table.jsx
+++ src/modules/clients/components/data-table.jsx
@@ -1,9 +1,21 @@
+'use client'
+
+import ClientSheet from '@modules/clients/components/client-sheet'
 import DataTableToolbar from '@modules/clients/components/data-table-toolbar'
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/modules/commons/ui/table'
 
 export default function DataTable({ data }) {
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectedClient, setSelectedClient] = useState(null)
+
+  const handleRowClick = (client) => {
+    setSelectedClient(client)
+    setIsOpen(true)
+  }
+
   return (
     <div className="space-y-4">
       <DataTableToolbar />
@@ -19,7 +31,7 @@ export default function DataTable({ data }) {
           </TableHeader>
           <TableBody>
             {data.map((client) => (
-              <TableRow key={client.id}>
+              <TableRow key={client.id} onClick={() => handleRowClick(client)}>
                 <TableCell className="font-medium">{client.name}</TableCell>
                 {/* <TableCell>{client.contact.size}</TableCell> */}
                 <TableCell className="text-right">{client.active_order}</TableCell>
@@ -30,6 +42,7 @@ export default function DataTable({ data }) {
           </TableBody>
         </Table>
       </div>
+      <ClientSheet isOpen={isOpen} onOpenChange={setIsOpen} client={selectedClient} />
     </div>
   )
 }
